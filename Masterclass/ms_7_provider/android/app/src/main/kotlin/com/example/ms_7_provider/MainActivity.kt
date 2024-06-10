570861623615228a483b384794c5d65735c84c3f
Abdul Rahman Haroon <abdulrahman.haroonr@gmail.com>
2024-06-10 13:24:30 +0300

    Provider

---
+++ Masterclass/ms_7_provider/android/app/src/main/kotlin/com/example/ms_7_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ms_7_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Masterclass/ms_7_provider/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ms_7_provider/counter_model.dart';
+import 'package:ms_7_provider/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp()
+    ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
