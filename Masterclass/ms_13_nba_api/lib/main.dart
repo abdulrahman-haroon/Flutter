441598b2f2f2c098c549355a2857bbd7062ab549
abdulrahman-haroon <abdulrahman.haroonr@gmail.com>
2024-06-19 01:51:29 +0300

    API

---
+++ Masterclass/ms_13_nba_api/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ms_13_nba_api/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
